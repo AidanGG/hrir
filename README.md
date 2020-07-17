e6a3cdca995b3097b6b38fe97502c16002cdafb8
Max Maisel <max.maisel@posteo.de>
2020-07-17 20:04:53 +0200

    Add support for HRIRs from TU-Berlin and add built-in equalizer.

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 ## PulseAudio virtual surround HRIR builder
 
-This script builds a 7.1 HRIR for PulseAudios
-[module-virtual-surround-sink](https://www.freedesktop.org/wiki/Software/PulseAudio/Documentation/User/Modules/#module-virtual-surround-sink)
-from the LISTEN Database.
+This script builds a 7.1 surround HRIR for PulseAudios
+[module-virtual-surround-sink](https://www.freedesktop.org/wiki/Software/PulseAudio/Documentation/User/Modules/#module-virtual-surround-sink).
 
 ### Dependencies
 * GNU Octave
 * FFMpeg
 
 ### Usage
+Download a HRIR database from a source of your choice. Currently supported are:
+
+#### LISTEN Database
 Select a measurement data set by listening to different demo sounds
 [here](http://recherche.ircam.fr/equipes/salles/listen/sounds.html).
 Ideally, the demo sound should listen like a noise that moves in a circle
@@ -17,8 +19,13 @@ around your head.
 Then download and extract the measurement data from the
 [LISTEN Database](http://recherche.ircam.fr/equipes/salles/listen/download.html).
 
+#### Measurements from TU Berlin
+Download a .mat-File from [TU-Berlin](https://dev.qu.tu-berlin.de/projects/measurements/repository).
+
+#### Script settings
 Adjust the variables "azim" and "elev" in the script to configure your
 preferred speaker locations and set the filename of the measurement data to use.
+See comments in the script as well.
 
 Finally, run `octave build_hrir.m`.
 This will produce a file `hrir.wav` in the current directory.
--- build_hrir.m
+++ build_hrir.m
@@ -3,30 +3,90 @@
 # Channel order of the following values
 channels={"front_left", "front_center", "front_right", ...
   "side_left", "side_right", "rear_left", "rear_right", "lfe"};
+
+## Settings
 # Azimut angles
 azim = [45 0 -45 90 -90 135 -135 180];
 # Elevation angles
 elev = [0 0 0 0 0 0 0 0];
+
 # Listen database file from
 # http://recherche.ircam.fr/equipes/salles/listen/download.html
 # It is located at the path "COMPENSATED/MAT/HRIR/" in the zip file.
 listen_mat="IRC_1002_C_HRIR.mat"
 
-load(listen_mat);
+# Alternatively, HRIRs from
+# https://dev.qu.tu-berlin.de/projects/measurements/repository/show/2010-11-kemar-anechoic/mat
+# can be used.
+#tub_mat="QU_KEMAR_anechoic_1m.mat"
+
+# Apply equalizer to result
+equalize_result = false;
+
+# Equalizer settings
+eq_freq = [0  30  60 120 250 500 1000 2000 4000 8000 16000 22100];
+eq_val  = [1   1   4   1   1   2    3    4    3    2     5     3];
+
+## Processing
+function x = load_listen(mat)
+  load(mat);
+  x.fs = l_eq_hrir_S.sampling_hz;
+  x.azim = l_eq_hrir_S.azim_v;
+  x.elev = l_eq_hrir_S.elev_v;
+  x.data = l_eq_hrir_S.content_m;
+end
+
+function x = load_tub(mat)
+  load(mat);
+  x.fs = irs.fs;
+  x.azim = mod(irs.apparent_azimuth/pi*180, 360);
+  x.elev = mod(irs.apparent_elevation/pi*180, 360);
+  x.data = irs.left.';
+end
+
+if exist("listen_mat") == 1
+  meas = load_listen(listen_mat);
+elseif exist("tub_mat") == 1
+  meas = load_tub(tub_mat);
+else
+  printf("Either listen_mat or tub_mat must be specified\n");
+  return
+end
 
 azim = mod(azim, 360);
-indices = (1:1:length(l_eq_hrir_S.elev_v));
+indices = (1:1:length(meas.elev));
 max_len=128;
 channel_count = length(channels);
 
-for k=1:1:channel_count
-  elev_idx = indices(l_eq_hrir_S.elev_v == elev(k));
-  azim_idx = indices(l_eq_hrir_S.azim_v == azim(k));
-  idx = intersect(azim_idx, elev_idx);
-  hrir = l_eq_hrir_S.content_m(idx,1:max_len);
+eq_tf = interp1(eq_freq, eq_val,
+  (1:1:max_len/2)*meas.fs/max_len/2);
+eq_ir = real(ifft(cat(2, 0, eq_tf, fliplr(eq_tf(1:length(eq_tf)-1)))));
+eq_ir = eq_ir / max(abs(eq_ir));
 
+#plot((1:1:128)*22100/128,eq_tf); hold on; plot(eq_freq, eq_val, 'ro'); hold off
+
+if equalize_result
+  hrirs = zeros(channel_count, 2*max_len-1);
+else
+  hrirs = zeros(channel_count, max_len);
+end
+
+for k=1:1:channel_count
+  elev_idx = indices(meas.elev == elev(k));
+  azim_idx = indices(meas.azim == azim(k));
+  idx = intersect(azim_idx, elev_idx);
+  hrir = meas.data(idx,1:max_len);
+  if equalize_result == true
+    hrir = conv(eq_ir, hrir);
+  end
+  hrirs(k,:) = hrir
+end
+
+hrir_max = max(max(abs(hrirs)));
+
+for k=1:1:channel_count
   audiowrite(cstrcat(channels{k}, ".wav"), ...
-    hrir, l_eq_hrir_S.sampling_hz);
+    hrir/hrir_max, meas.fs);
 end
 
 % Assemble it (octave fails channel mapping so use ffmpeg)
